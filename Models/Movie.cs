ad2016f13ef90d55b2c222045eed1109a748feee
BsKPLu <bianshuiguangzhong@gmail.com>
2024-04-29 17:22:24 +0800

    搜索功能添加

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RazorPagesMovie.Models
 {
@@ -7,9 +8,13 @@ namespace RazorPagesMovie.Models
         public int ID { get; set; }
         public string Title { get; set; }
 
+        [Display(Name ="Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public String Genre { get; set; }
+
+        //Price字段映射到数据库中的货币
+        [Column(TypeName ="decimal(18,2)")]
         public decimal Price {  get; set; }
     }
 }
--- Pages/Movies/Index.cshtml.cs
+++ Pages/Movies/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RazorPagesMovie.Data;
 using RazorPagesMovie.Models;
@@ -18,14 +19,29 @@ namespace RazorPagesMovie.Pages.Movies
         {
             _context = context;
         }
-
+        //当 OnGet 返回 void 或 OnGetAsync 返回 Task 时，不使用任何返回语句。 当返回类型是 IActionResult 或
+        //Task<IActionResult> 时，必须提供返回语句
+        //这里是数据查询界面，不需要作跳转页面操作，但在添加页面，添加完成后需要跳转到数据查询展示页面
         public IList<Movie> Movie { get;set; } = default!;
+
+        [BindProperty(SupportsGet =true)]
+        public string SearchString {  get; set; }
+        public SelectList Genres { get; set; }
+
+        [BindProperty(SupportsGet =true)]
+        public string MovieGenre {  get; set; }
 
         public async Task OnGetAsync()
         {
+            var movies=from m in _context.Movie select m;
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                movies = movies.Where(s => s.Title.Contains(SearchString));
+            }
+
             if (_context.Movie != null)
             {
-                Movie = await _context.Movie.ToListAsync();
+                Movie = await movies.ToListAsync();
             }
         }
     }
